72db1027c33419caae4792e98c32f9c55e437f52
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2022-11-21 11:47:32 -0300

    Add getByTestId y Props

--- tests/FirstApp.test.jsx
+++ tests/FirstApp.test.jsx
@@ -1,29 +1,35 @@
-import { render } from '@testing-library/react'
-import { FirstApp } from '../src/FirstApp'
+import {render} from "@testing-library/react";
+import {FirstApp} from "../src/FirstApp";
 
-describe('Pruebas en <FirstApp />', () => {
+describe("Pruebas en <FirstApp />", () => {
+	const title = "Hola soy un Titulo";
+	const subTitle = "Hola soy un subtitulo";
 
-  const title = 'Hola soy un Titulo';
+	// test('Debe hacer match con el snapshot', () => {
 
+	//   const {container} = render(<FirstApp title={title} />);
 
-  test('Debe hacer match con el snapshot', () => {
+	//   expect(container).toMatchSnapshot();
 
-    const {container} = render(<FirstApp title={title} />);
-    
-    expect(container).toMatchSnapshot();
+	// })
 
-  })
+	test("Debe mostar el titulo en un h1", () => {
+		const {getByText, getByTestId} = render(<FirstApp title={title} />);
 
-  test('Debe mostar el titulo en un h1', () => {
-    
-    const { container, getByText } = render(<FirstApp title={title} />);
-    
-    expect(getByText(title)).toBeTruthy();
+		expect(getByText(title)).toBeTruthy();
 
-    // Buscar si existe un h1 con JS
-    const h1 = container.querySelector('h1');
-    expect(h1.innerHTML).toContain(title);
-  
-  })
+		// Buscar si existe un h1 con JS
+		// const h1 = container.querySelector('h1');
+		// expect(h1.innerHTML).toContain(title);
 
-})
+		expect(getByTestId("test-title")).toBeTruthy();
+		expect(getByTestId("test-title").innerHTML).toBe(title);
+		expect(getByTestId("test-title").innerHTML).toContain(title);
+	});
+
+	test("Debe mostar el subtitulo enviado por props", () => {
+		const {getAllByText} = render(<FirstApp title={title} subTitle={subTitle} />);
+
+    expect(getAllByText(subTitle).length).toBe(2);
+	});
+});
